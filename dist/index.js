7f0ef6ebc6eb23ae9338948e45e319fcc27980c2
Vova <vovanishshkrobot29@gmail.com>
2023-07-27 01:58:38 +0300

    game done

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,56 @@
 const canvas = document.querySelector('#myCanvas');
 const ctx = canvas.getContext('2d');
 class GameMap {
-    spawnCharacter(character) {
-        character.texture.onload = function () {
-            ctx.drawImage(character.texture, character.position.x, character.position.y);
+    constructor({ Character }) {
+        this.mobs = [];
+        this.Character = new Character({
+            img: './images/character.png',
+            position: {
+                x: canvas.width / 2,
+                y: canvas.height / 2
+            },
+            Weapon: Gun,
+            mobs: this.mobs
+        });
+        ;
+        this.spawnCharacter();
+        document.addEventListener('keydown', (event) => {
+            this.Character.move(event.key);
+            if (event.key === 'x') {
+                this.Character.attack();
+            }
+        });
+    }
+    spawnCharacter() {
+        this.Character.texture.onload = () => {
+            ctx.drawImage(this.Character.texture, this.Character.position.x, this.Character.position.y);
+        };
+    }
+    spawnMob(Mob, position) {
+        const mob = new Mob({
+            position: {
+                x: position.x || 900,
+                y: position.y || 900
+            },
+            enemy: this.Character,
+            speed: 1,
+        });
+        this.mobs.push(mob);
+        mob.texture.onload = () => {
+            ctx.drawImage(mob.texture, mob.position.x, mob.position.y);
+            mob.move();
         };
     }
 }
 class Character {
-    constructor({ position, img, weapon }) {
+    constructor({ position, img, Weapon, mobs }) {
         this.position = position;
         this.texture = new Image();
         this.texture.src = img;
-        this.weapon = weapon;
+        this.mobs = mobs;
+        this.weapon = new Weapon({ range: 50, dmg: 17, mobs });
         this.attacking = false;
+        this.died = false;
         this.drawWeapon();
     }
     drawWeapon() {
@@ -22,22 +59,29 @@ class Character {
             ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y);
         };
     }
+    die() {
+        this.died = true;
+        ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
+    }
     move(direction) {
-        if (this.attacking) {
+        if (this.attacking || this.died) {
             return;
         }
         const moveDistance = 10;
-        ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
         if (direction === 'ArrowLeft') {
+            ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
             this.position.x -= moveDistance;
         }
         else if (direction === 'ArrowRight') {
+            ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
             this.position.x += moveDistance;
         }
         else if (direction === 'ArrowUp') {
+            ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
             this.position.y -= moveDistance;
         }
         else if (direction === 'ArrowDown') {
+            ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
             this.position.y += moveDistance;
         }
         else {
@@ -47,79 +91,145 @@ class Character {
         ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y);
     }
     attack() {
-        this.attacking = true;
-        ctx.save();
-        ctx.translate(this.position.x + 50, this.position.y);
-        ctx.rotate(Math.PI / 4);
-        ctx.translate(-this.position.x + 50, -this.position.y);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(this.texture, this.position.x, this.position.y);
-        ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y);
-        ctx.restore();
-        setTimeout(() => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(this.texture, this.position.x, this.position.y);
-            ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y);
-            this.attacking = false;
-        }, 250);
+        if (this.died) {
+            return;
+        }
+        this.weapon.attack({ x: this.position.x + 50, y: this.position.y });
+        // this.attacking = true
+        // ctx.save();
+        // ctx.translate(this.position.x + 50, this.position.y);
+        // ctx.rotate(Math.PI / 4);
+        // ctx.translate(-this.position.x + 50, -this.position.y);
+        // ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // ctx.drawImage(this.texture, this.position.x, this.position.y);
+        // ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y);
+        // ctx.restore();
+        // setTimeout(() => {
+        //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //   ctx.drawImage(this.texture, this.position.x, this.position.y);
+        //   ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y)
+        //   this.attacking = false
+        // }, 250)
     }
     changeWeapon(weapon) {
     }
 }
 class Weapon {
-    constructor({ dmg, range, img }) {
+    constructor({ dmg, range, mobs }) {
         this.dmg = dmg;
         this.range = range;
         this.texture = new Image();
-        this.texture.src = img;
+        this.mobs = mobs;
     }
 }
 class Sword extends Weapon {
     attack() {
     }
 }
-const sword = new Sword({
-    dmg: 17,
-    range: 50,
-    img: './images/sword.png'
-});
-const character = new Character({
-    img: './images/character.png',
-    position: {
-        x: canvas.width / 2,
-        y: canvas.height / 2
-    },
-    weapon: sword
-});
-// const sword2 = new Sword({
-//   dmg: 17,
-//   range: 50,
-//   img: './images/sword.png'
-// });
-// const character2 = new Character({
-//   img: './images/character.png',
-//   position: {
-//     x: 100,
-//     y: 100
-//   },
-//   weapon: sword2
-// });
-const mainMap = new GameMap();
-mainMap.spawnCharacter(character);
-// mainMap.spawnCharacter(character2);
-document.addEventListener('keydown', function (event) {
-    character.move(event.key);
-    if (event.key === 'x') {
-        console.log(1);
-        character.attack();
+class Gun extends Weapon {
+    constructor({ dmg, range, mobs }) {
+        super({ dmg, range, mobs });
+        this.texture.src = './images/gun.png';
     }
-    // if (event.key === 'a') {
-    //   character2.move('ArrowLeft');
-    // } else if (event.key === 'd') {
-    //   character2.move('ArrowRight');
-    // } else if (event.key === 'w') {
-    //   character2.move('ArrowUp');
-    // } else if (event.key === 's') {
-    //   character2.move('ArrowDown');
-    // }
-});
+    attack({ x: xP, y: yP }) {
+        let x = xP + 150;
+        const y = yP + 10;
+        const speed = 2;
+        // Функция для рисования шарика
+        function drawBall() {
+            ctx.clearRect(x + speed - 50, y - 20, 40, 40); // Очистить холст
+            ctx.beginPath();
+            ctx.arc(x, y, 20, 0, Math.PI * 2);
+            ctx.fillStyle = 'blue';
+            ctx.fill();
+            ctx.closePath();
+        }
+        // Функция анимации
+        const animate = () => {
+            x += speed; // Увеличиваем координату x на значение скорости
+            drawBall(); // Рисуем шарик
+            const damagedMobIndex = this.mobs.findIndex(({ position, texture }) => {
+                const halfOfMobWidth = texture.width / 2;
+                const halfOfMobHeight = texture.height / 2;
+                return (position.x - halfOfMobWidth < x && position.x + halfOfMobWidth > x) && (position.y - halfOfMobHeight < y && position.y + halfOfMobHeight + 40 > y);
+            });
+            if (damagedMobIndex > -1) {
+                const [damagedMob] = this.mobs.splice(damagedMobIndex, 1);
+                damagedMob.die();
+                ctx.clearRect(x + speed - 22, y - 20, 40, 40);
+                return;
+            }
+            if (canvas.width > x || 0 > canvas.width) {
+                requestAnimationFrame(animate);
+            }
+            else {
+                ctx.clearRect(x + speed - 40, y - 20, 40, 40);
+            }
+        };
+        // Запускаем анимацию
+        animate();
+    }
+}
+class Mob {
+    constructor({ position, enemy, speed }) {
+        this.position = position;
+        this.enemy = enemy;
+        this.speed = speed;
+        this.texture = new Image();
+        this.died = false;
+    }
+    die() {
+        this.died = true;
+    }
+    move() {
+        const drawMob = () => {
+            // ctx.clearRect(this.position.x + this.speed, this.position.y, 80, 53); // Очистить холст
+            ctx.drawImage(this.texture, this.position.x, this.position.y);
+        };
+        const animate = () => {
+            ctx.clearRect(this.position.x + this.speed - 10, this.position.y, 110, 53);
+            if (this.enemy.position.x < this.position.x) {
+                this.position.x -= this.speed;
+            }
+            else {
+                this.position.x += this.speed;
+            }
+            if (this.enemy.position.y < this.position.y) {
+                this.position.y -= this.speed;
+            }
+            else {
+                this.position.y += this.speed;
+            }
+            const halfOfMobWidth = this.texture.width / 2;
+            const halfOfMobHeight = this.texture.height / 2;
+            if ((this.enemy.position.x - halfOfMobWidth < this.position.x && this.enemy.position.x + halfOfMobWidth > this.position.x) && (this.enemy.position.y - halfOfMobHeight < this.position.y && this.enemy.position.y + halfOfMobHeight > this.position.y)) {
+                this.enemy.die();
+            }
+            drawMob();
+            if (!this.died) {
+                setTimeout(() => {
+                    requestAnimationFrame(animate);
+                }, 15);
+            }
+            else {
+                ctx.clearRect(this.position.x + this.speed - 10, this.position.y, 110, 53);
+            }
+        };
+        animate();
+    }
+}
+class Fish extends Mob {
+    constructor({ position, enemy, speed }) {
+        super({ position, enemy, speed });
+        this.texture.src = './images/fish.jpg';
+    }
+}
+const mainMap = new GameMap({ Character });
+// mainMap.spawnMob(Fish, { x: 1800, y: Math.floor(Math.random() * 1081) });
+let spawnTime = 3000;
+setInterval(() => {
+    spawnTime = spawnTime - spawnTime / 10;
+}, 10000);
+setInterval(() => {
+    mainMap.spawnMob(Fish, { x: Math.floor(Math.random() * (1920 - 960 + 1)) + 960, y: Math.floor(Math.random() * 1081) });
+}, spawnTime);
--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,55 @@ const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
 interface IWeapon {
   dmg: number
   range: number
-  texture: HTMLImageElement,
+  texture: HTMLImageElement
   attack: () => void
 }
 
 class GameMap {
-  spawnCharacter(character: Character) {
-    character.texture.onload = function () {
-      ctx.drawImage(character.texture, character.position.x, character.position.y);
+  mobs: Mob[];
+  Character: Character
+  constructor({ Character }: { Character: { new(...args: any[]): Character } }) {
+    this.mobs = [];
+    this.Character = new Character({
+      img: './images/character.png',
+      position: {
+        x: canvas.width / 2,
+        y: canvas.height / 2
+      },
+      Weapon: Gun,
+      mobs: this.mobs
+    });;
+    this.spawnCharacter();
+
+    document.addEventListener('keydown', (event) => {
+      this.Character.move(event.key);
+      if (event.key === 'x') {
+        this.Character.attack();
+      }
+    });
+  }
+
+  private spawnCharacter() {
+    this.Character.texture.onload = () => {
+      ctx.drawImage(this.Character.texture, this.Character.position.x, this.Character.position.y);
+    };
+  }
+
+  spawnMob(Mob: { new(arg: { position: { x: number, y: number }, enemy: Character, speed: number }): Mob }, position: { x: number, y: number }) {
+    const mob = new Mob({
+      position: {
+        x: position.x || 900,
+        y: position.y || 900
+      },
+      enemy: this.Character,
+      speed: 1,
+
+    });
+
+    this.mobs.push(mob);
+    mob.texture.onload = () => {
+      ctx.drawImage(mob.texture, mob.position.x, mob.position.y);
+      mob.move();
     };
   }
 }
@@ -22,14 +63,18 @@ class Character {
   position: { x: number, y: number }
   texture: HTMLImageElement
   weapon: IWeapon
+  mobs: Mob[]
+  died: boolean
   private attacking: boolean
 
-  constructor({ position, img, weapon }: { position: { x: number, y: number }, img: string, weapon: IWeapon }) {
+  constructor({ position, img, Weapon, mobs }: { position: { x: number, y: number }, img: string, Weapon: { new(arg: { range: number, dmg: number }): IWeapon }, mobs: Mob[] }) {
     this.position = position;
     this.texture = new Image();
     this.texture.src = img;
-    this.weapon = weapon;
+    this.mobs = mobs;
+    this.weapon = new Weapon({ range: 50, dmg: 17, mobs });
     this.attacking = false;
+    this.died = false
     this.drawWeapon();
   }
 
@@ -39,19 +84,27 @@ class Character {
     };
   }
 
+  die() {
+    this.died = true
+    ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
+  }
+
   move(direction: string) {
-    if (this.attacking) {
+    if (this.attacking || this.died) {
       return
     }
     const moveDistance = 10;
-    ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
     if (direction === 'ArrowLeft') {
+      ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
       this.position.x -= moveDistance;
     } else if (direction === 'ArrowRight') {
+      ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
       this.position.x += moveDistance;
     } else if (direction === 'ArrowUp') {
+      ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
       this.position.y -= moveDistance;
     } else if (direction === 'ArrowDown') {
+      ctx.clearRect(this.position.x + 10, this.position.y, this.texture.width + this.weapon.texture.width - 30, this.weapon.texture.height);
       this.position.y += moveDistance;
     } else {
       return;
@@ -61,23 +114,26 @@ class Character {
   }
 
   attack() {
+    if (this.died) {
+      return
+    }
+    this.weapon.attack({ x: this.position.x + 50, y: this.position.y });
+    // this.attacking = true
+    // ctx.save();
+    // ctx.translate(this.position.x + 50, this.position.y);
+    // ctx.rotate(Math.PI / 4);
+    // ctx.translate(-this.position.x + 50, -this.position.y);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.drawImage(this.texture, this.position.x, this.position.y);
+    // ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y);
+    // ctx.restore();
 
-    this.attacking = true
-    ctx.save();
-    ctx.translate(this.position.x + 50, this.position.y);
-    ctx.rotate(Math.PI / 4);
-    ctx.translate(-this.position.x + 50, -this.position.y);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(this.texture, this.position.x, this.position.y);
-    ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y);
-    ctx.restore();
-
-    setTimeout(() => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(this.texture, this.position.x, this.position.y);
-      ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y)
-      this.attacking = false
-    }, 250)
+    // setTimeout(() => {
+    //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //   ctx.drawImage(this.texture, this.position.x, this.position.y);
+    //   ctx.drawImage(this.weapon.texture, this.position.x + 50, this.position.y)
+    //   this.attacking = false
+    // }, 250)
   }
 
   changeWeapon(weapon: IWeapon) {
@@ -89,12 +145,13 @@ abstract class Weapon implements IWeapon {
   dmg: number
   range: number
   texture: HTMLImageElement
+  protected mobs: Mob[]
 
-  constructor({ dmg, range, img }: { dmg: number, range: number, img: string }) {
+  constructor({ dmg, range, mobs }: { dmg: number, range: number, mobs: Mob[] }) {
     this.dmg = dmg;
     this.range = range;
     this.texture = new Image();
-    this.texture.src = img;
+    this.mobs = mobs;
   }
 
 
@@ -108,53 +165,131 @@ class Sword extends Weapon {
   }
 }
 
+class Gun extends Weapon {
 
-const sword = new Sword({
-  dmg: 17,
-  range: 50,
-  img: './images/sword.png'
-});
-const character = new Character({
-  img: './images/character.png',
-  position: {
-    x: canvas.width / 2,
-    y: canvas.height / 2
-  },
-  weapon: sword
-});
-
-// const sword2 = new Sword({
-//   dmg: 17,
-//   range: 50,
-//   img: './images/sword.png'
-// });
-// const character2 = new Character({
-//   img: './images/character.png',
-//   position: {
-//     x: 100,
-//     y: 100
-//   },
-//   weapon: sword2
-// });
-
-const mainMap = new GameMap();
-mainMap.spawnCharacter(character);
-// mainMap.spawnCharacter(character2);
-
-document.addEventListener('keydown', function (event) {
-  character.move(event.key);
-  if (event.key === 'x') {
-    console.log(1)
-    character.attack();
+  constructor({ dmg, range, mobs }: { dmg: number, range: number, mobs: Mob[] }) {
+    super({ dmg, range, mobs });
+    this.texture.src = './images/gun.png';
   }
 
-  // if (event.key === 'a') {
-  //   character2.move('ArrowLeft');
-  // } else if (event.key === 'd') {
-  //   character2.move('ArrowRight');
-  // } else if (event.key === 'w') {
-  //   character2.move('ArrowUp');
-  // } else if (event.key === 's') {
-  //   character2.move('ArrowDown');
-  // }
-});
+  attack({ x: xP, y: yP }) {
+    let x = xP + 150;
+    const y = yP + 10;
+    const speed = 2;
+
+    // Функция для рисования шарика
+    function drawBall() {
+      ctx.clearRect(x + speed - 50, y - 20, 40, 40); // Очистить холст
+      ctx.beginPath();
+      ctx.arc(x, y, 20, 0, Math.PI * 2);
+      ctx.fillStyle = 'blue';
+      ctx.fill();
+      ctx.closePath();
+    }
+
+    // Функция анимации
+    const animate = () => {
+      x += speed; // Увеличиваем координату x на значение скорости
+      drawBall(); // Рисуем шарик
+      const damagedMobIndex = this.mobs.findIndex(({ position, texture }) => {
+        const halfOfMobWidth = texture.width / 2;
+        const halfOfMobHeight = texture.height / 2;
+        return (position.x - halfOfMobWidth < x && position.x + halfOfMobWidth > x) && (position.y - halfOfMobHeight < y && position.y + halfOfMobHeight + 40 > y);
+      })
+      if (damagedMobIndex > -1) {
+        const [damagedMob] = this.mobs.splice(damagedMobIndex, 1);
+        damagedMob.die();
+        ctx.clearRect(x + speed - 22, y - 20, 40, 40);
+        return
+      }
+
+      if (canvas.width > x || 0 > canvas.width) {
+        requestAnimationFrame(animate);
+      } else {
+        ctx.clearRect(x + speed - 40, y - 20, 40, 40);
+      }
+    }
+
+    // Запускаем анимацию
+    animate();
+  }
+}
+
+abstract class Mob {
+
+  position: { x: number, y: number }
+  enemy: Character
+  speed: number
+  texture: HTMLImageElement
+  died: boolean
+
+  constructor({ position, enemy, speed }: { position: { x: number, y: number }, enemy: Character, speed: number }) {
+    this.position = position;
+    this.enemy = enemy;
+    this.speed = speed;
+    this.texture = new Image();
+    this.died = false
+  }
+
+  die() {
+    this.died = true;
+  }
+
+  move() {
+    const drawMob = () => {
+      // ctx.clearRect(this.position.x + this.speed, this.position.y, 80, 53); // Очистить холст
+      ctx.drawImage(this.texture, this.position.x, this.position.y);
+    }
+
+    const animate = () => {
+      ctx.clearRect(this.position.x + this.speed - 10, this.position.y, 110, 53);
+      if (this.enemy.position.x < this.position.x) {
+        this.position.x -= this.speed;
+      } else {
+        this.position.x += this.speed;
+      }
+
+      if (this.enemy.position.y < this.position.y) {
+        this.position.y -= this.speed;
+      } else {
+        this.position.y += this.speed;
+      }
+
+      const halfOfMobWidth = this.texture.width / 2;
+      const halfOfMobHeight = this.texture.height / 2;
+      if ((this.enemy.position.x - halfOfMobWidth < this.position.x && this.enemy.position.x + halfOfMobWidth > this.position.x) && (this.enemy.position.y - halfOfMobHeight < this.position.y && this.enemy.position.y + halfOfMobHeight > this.position.y)) {
+        this.enemy.die();
+      }
+
+      drawMob();
+      if (!this.died) {
+        setTimeout(() => {
+          requestAnimationFrame(animate);
+        }, 15)
+      } else {
+        ctx.clearRect(this.position.x + this.speed - 10, this.position.y, 110, 53);
+      }
+    }
+    animate();
+  }
+}
+
+class Fish extends Mob {
+  constructor({ position, enemy, speed }: { position: { x: number, y: number }, enemy: Character, speed: number }) {
+    super({ position, enemy, speed });
+    this.texture.src = './images/fish.jpg';
+  }
+}
+
+const mainMap = new GameMap({ Character });
+// mainMap.spawnMob(Fish, { x: 1800, y: Math.floor(Math.random() * 1081) });
+
+let spawnTime = 3000;
+
+setInterval(() => {
+  spawnTime = spawnTime - spawnTime / 10;
+}, 10000)
+
+setInterval(() => {
+  mainMap.spawnMob(Fish, { x: Math.floor(Math.random() * (1920 - 960 + 1)) + 960, y: Math.floor(Math.random() * 1081) });
+}, spawnTime)
